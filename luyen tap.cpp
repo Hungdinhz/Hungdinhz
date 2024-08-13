b7a7eb5b4faae0bc96110d5865950bae62f4b18f
Hungdinhz <phunghung12112005@gmail.com>
2024-08-13 20:39:14 +0700

    Add files via upload

---
+++ luyen tap.cpp
@@ -0,0 +1,24 @@
+ts makets();
+ts makets_file(char ma[],char ten[], Ngaysinh ns, char qq[], Diem d);
+int Size(ts a);
+void nhap_file(ts *head);
+void themdau(ts *head);
+void themcuoi(ts *head);
+void thembatki(ts *head,int *pos);
+void xoa_dau(ts *head);
+void xoa_cuoi(ts *head);
+void xoa_bat_ki(ts *head,int *pos);
+void sap_xep_tang(ts *head);
+void sap_xep_giam(ts *head);
+void thong_tin(Thisinh s,int i);
+void in_du_lieu(ts head);
+void tim_kiem(ts head);
+void cap_nhat_thong_tin(ts head, char ma[]);
+void luu_vao_file(ts head);
+float tinh_diem_trung_binh(ts head);
+void time_thi_sinh_theo_que(ts head, char quequan[]);
+float diem_max(ts head);
+void in_thi_sinh_diem_cao_nhat(ts head);
+float diem_min(ts head);
+void in_thi_sinh_diem_thap_nhat(ts head);
+
